9f300ce950d9d4a4653faeb336dfd2cc0552f390
Luis Eduardo Hinojos Torres <eduardhtl84@gmail.com>
2020-12-21 16:24:51 -0700

    visor cocinero

---
+++ Pantallamesero/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gecko;
+
+namespace Pantallamesero
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Xpcom.Initialize("Firefox");
+            geckoWebBrowser1.Navigate("https://restauranteqr17.000webhostapp.com/indexm.php");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            geckoWebBrowser1.Reload();
+        }
+    }
+}
